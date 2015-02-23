a01674ba90f3eff2e7c255f9f40e189f0c1a0fe4
Keith Yong <terda12@gmail.com>
2015-02-23 14:31:46 -0500

    Added execution time, combined both functions into one

--- epi6-3.cpp
+++ epi6-3.cpp
@@ -1,13 +1,19 @@
 #include <string>
 #include <vector>
 #include <iostream>
+#include <chrono>
 using std::string;
 using std::vector;
 using std::cout;
 using std::endl;
 
+/* O(1) */
 int getInt(const char &c) {
     return c - '0';
+}
+
+void push_front(string &str, char c) {
+    str.insert(0, 1, c);
 }
 
 void printVector(vector<string> S) {
@@ -17,10 +23,8 @@ void printVector(vector<string> S) {
     }
 }
 
-void push_front(string &str, char c) {
-    str.insert(0, 1, c);
-}
-//=====================================
+/* Grade school multiplication, except it doesn't sum the results,
+ * just returns a vector<string> that needs to be summed. */
 vector<string> mult(string s1, string s2, bool &isNeg) {
     isNeg = false;
     if (s1[0] == '-' && s2[0] != '-') {
@@ -61,7 +65,7 @@ vector<string> mult(string s1, string s2, bool &isNeg) {
             a = x % 10;
             carry = (x - a) / 10;
             push_front(result, a + '0');
-            if (bg_it== bigger.rend() - 1 && carry != 0){
+            if (bg_it == bigger.rend() - 1 && carry != 0){
                 push_front(result, carry + '0');
             } 
         }
@@ -100,7 +104,7 @@ string sum(vector<string> &A, bool isNeg) {
         x = 0;
         x += carry;
         for (it =A.begin(); it != A.end();it++ ){
-            x+= getInt((*it)[i]);
+            x += getInt((*it)[i]);
         } 
         a = x % 10;
         carry = (x - a) / 10;
@@ -115,10 +119,20 @@ string sum(vector<string> &A, bool isNeg) {
     return result;
 }
 
+/* Combining mult + sum */
+string multiply(string s1, string s2) {
+    string result;
+    bool neg;
+    vector<string> A;
+    A = mult(s1, s2, neg);
+    result = sum(A, neg);
+    return result;
+}
+
 
 int main() {
-    vector<string> my_A;
-    bool neg;
-    my_A = mult("211333333333333333333333333333333311111111111", "2133333333333333312222222222444444444444444444444422222222222222233333333333333333333333333377777777777777777777777777", neg);
-    cout << sum(my_A, neg) << endl;
+    const auto start_time = std::chrono::steady_clock::now();
+    cout << multiply("4589438593845983945893485938495893485982934859829348592843958928349582943589284359829438592849582945839127387128378127845832678568745624987435692837465928736459872364957826349587629348756923847569283475692384756927438569278345692876345", "12938912839189489452734857209834750298347502983475736285927460192634017634017250194755108475017894750298374053847503984573328349823948928349289849283409180298417465298374659287436598273465982763495872634756734657637567365763576375637657") << endl;
+    double time_in_seconds = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::steady_clock::now() - start_time).count() / 1000.0;
+    cout << "Executed in " << time_in_seconds << " seconds." << endl;
 }
